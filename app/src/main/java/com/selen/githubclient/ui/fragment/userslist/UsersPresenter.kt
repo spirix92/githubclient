c1449a655b769eb69cc6ec49a61f2e8c6a4705f4
Aleksey Pyaterko <pyaterko92@mail.ru>
2020-11-14 17:30:36 +0300

    hw 3

--- app/src/main/java/com/selen/githubclient/ui/fragment/userslist/UsersPresenter.kt
+++ app/src/main/java/com/selen/githubclient/ui/fragment/userslist/UsersPresenter.kt
@@ -3,6 +3,7 @@ package com.selen.githubclient.ui.fragment.userslist
 import com.selen.githubclient.model.GithubUsersRepo
 import com.selen.githubclient.ui.fragment.userslist.adapter.UsersListPresenter
 import com.selen.githubclient.ui.navigator.Screens
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
@@ -12,23 +13,31 @@ import ru.terrakok.cicerone.Router
 class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router) : MvpPresenter<UsersView>() {
 
     val usersListPresenter = UsersListPresenter()
+    val compositeDisposable = CompositeDisposable()
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
-        loadData()
+        asyncLoadData()
 
         usersListPresenter.itemClickListener = { itemView ->
             router.navigateTo(Screens.UserDetailScreen(usersListPresenter.users[itemView.pos]))
         }
     }
 
-    fun loadData() {
-        val users = usersRepo.getUsers()
-        usersListPresenter.users.addAll(users)
-        viewState.updateList()
+    fun asyncLoadData() {
+        compositeDisposable.add(usersRepo.getObservableUsers().subscribe({ s ->
+            usersListPresenter.users.add(s)
+            viewState.updateList()
+        }, { e ->
+            println("onError: ${e.message}")
+        }))
     }
 
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
     fun backPressed(): Boolean {
         router.exit()
         return true
